3edb67bd2213bf611c8bf123d3cbb98a2587c68d
tcpip147 <tcpip147@gmail.com>
2024-10-22 07:21:57 +0900

    3

--- src/canvas/grid.ts
+++ src/canvas/grid.ts
@@ -27,6 +27,7 @@ class Grid {
 
   redraw() {
     this.ctx.clearRect(0, 0, this.fillWidth(), this.fillHeight());
+    this.strokeRect(this.getPixel(5, 5)[0], this.getPixel(5, 5)[1], 100, 100);
     this.drawRoller();
   }
 
@@ -37,6 +38,9 @@ class Grid {
 
     const digits = Math.round((this.fillHeight() + this.origin[1]) / 20).toString().length;
     this.left = 5 + digits * 5;
+
+    this.fillRect(0, 0, this.fillWidth(), 20);
+    this.fillRect(0, 0, 20 + this.left, this.fillHeight());
 
     this.ctx.beginPath();
     for (let i = 2; i < (this.fillWidth() + this.origin[0]) / 20; i++) {
@@ -78,6 +82,10 @@ class Grid {
   private crisp(pixel: number) {
     const halfThickness = this.ctx.lineWidth / 2;
     return this.ctx.lineWidth % 2 ? (Number.isInteger(pixel) ? pixel : Math.round(pixel + halfThickness)) - halfThickness : Math.round(pixel);
+  }
+
+  private getPixel(x: number, y: number): [number, number] {
+    return [this.left + 20 + 20 * x - this.origin[0], 20 + 20 * y - this.origin[1]];
   }
 
   //
